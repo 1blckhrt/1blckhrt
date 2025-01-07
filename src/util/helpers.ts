e1949b7b5451729d3a69c23c32f03e6213303459
1blckhrt <williams.1691@wright.edu>
2025-01-06 23:43:38 -0500

    docs: add docstrings to helpers

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,11 +1,17 @@
 import { LANYARD_URL, DATE_OF_BIRTH } from "./constants";
 
+/*
+ * Fetches my Discord Status via Lanyard.
+ */
 export async function GetStatus() {
   const response = await fetch(LANYARD_URL, { cache: "no-store" });
   const { data } = await response.json();
   return data.discord_status;
 }
 
+/*
+ * Calculates my age.
+ */
 export function calculateAge() {
   return new Date(Date.now() - DATE_OF_BIRTH.getTime()).getFullYear() - 1_970;
 }

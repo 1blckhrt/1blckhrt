9d720e05280cbef84f42951e25090ba3462f3ad1
1blckhrt <williams.1691@wright.edu>
2025-01-09 23:31:36 -0500

    feat: add about me

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import "./tailwind.css";
+import { calculateAge } from "./util/helpers";
+
+const age = calculateAge();
+
+const age_span_element = document.querySelector<HTMLSpanElement>("#age");
+if (age_span_element) {
+  age_span_element.textContent = age.toString();
+}

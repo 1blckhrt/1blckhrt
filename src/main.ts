fb53353a695cf84602094e941d519864b1da9990
1blckhrt <williams.1691@wright.edu>
2025-01-10 00:10:34 -0500

    feat: add current time, refactor calculate age

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import "./tailwind.css";
-import { calculateAge } from "./util/helpers";
+import { calculateAge, displayCurrentTime } from "./util/helpers";
 
-const age = calculateAge();
-
-const age_span_element = document.querySelector<HTMLSpanElement>("#age");
-if (age_span_element) {
-  age_span_element.textContent = age.toString();
-}
+document.addEventListener("DOMContentLoaded", () => {
+  calculateAge();
+  displayCurrentTime();
+});
--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,8 +1,27 @@
-import { DATE_OF_BIRTH } from "./constants";
+import { DATE_OF_BIRTH, TIME_FORMAT_OPTIONS } from "./constants";
 
 /*
  * Calculates my age.
  */
 export function calculateAge() {
-  return new Date(Date.now() - DATE_OF_BIRTH.getTime()).getFullYear() - 1_970;
+  const age = new Date(Date.now() - DATE_OF_BIRTH.getTime()).getFullYear() - 1_970;
+
+  const age_span_element = document.querySelector<HTMLSpanElement>("#age");
+  if (age_span_element) {
+    age_span_element.textContent = age.toString();
+  }
+}
+
+/*
+ * Displays my current time.
+ */
+export function displayCurrentTime() {
+  const now = new Date();
+
+  const formattedTime = new Intl.DateTimeFormat("en-US", TIME_FORMAT_OPTIONS).format(now);
+
+  const timeElement = document.querySelector<HTMLSpanElement>("#current-time");
+  if (timeElement) {
+    timeElement.textContent = formattedTime;
+  }
 }
